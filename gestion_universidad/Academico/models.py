c286ef15830c1453561b1275e77de81823a64a26
JuanGomez-14 <jmgomezc14@gmail.com>
2023-10-17 13:48:28 -0500

    Feat: se crea templates y urls en Academico

--- gestion_universidad/Academico/models.py
+++ gestion_universidad/Academico/models.py
@@ -5,3 +5,7 @@ class Curso(models.Model):
     codigo = models.CharField(primary_key=True, max_length=8)
     nombre = models.CharField(max_length=50)
     creditos = models.PositiveSmallIntegerField()
+
+    def __str__(self) -> str:
+        texto = "{0} ({1})"
+        return texto.format(self.nombre, self.creditos)
