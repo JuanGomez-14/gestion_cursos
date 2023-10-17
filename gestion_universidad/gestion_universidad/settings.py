f59644b425bf5ad4d8ab186bcc272ceec809114c
JuanGomez-14 <jmgomezc14@gmail.com>
2023-10-17 13:20:05 -0500

    Feat: se crea la aplicación Academico

--- gestion_universidad/gestion_universidad/settings.py
+++ gestion_universidad/gestion_universidad/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'gestion_universidad.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'universidad.db',
+        'NAME': 'universidad.db',
     }
 }
 
