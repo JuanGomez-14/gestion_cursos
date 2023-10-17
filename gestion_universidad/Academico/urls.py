f14b91302d4932391f53097c8b7c9f7fd2d6fd71
JuanGomez-14 <jmgomezc14@gmail.com>
2023-10-17 15:45:29 -0500

    Feat: Funcion editar ya funciona

--- gestion_universidad/Academico/urls.py
+++ gestion_universidad/Academico/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home),
     path('registrarCurso/', views.registrarCurso),
+    path('editarCurso/<codigo>', views.editarCurso),
+    path('edicionCurso/', views.edicionCurso),
     path('eliminarCurso/<codigo>', views.eliminarCurso)
 ]
--- gestion_universidad/Academico/views.py
+++ gestion_universidad/Academico/views.py
@@ -21,3 +21,20 @@ def eliminarCurso(request, codigo):
     curso = Curso.objects.get(codigo=codigo)
     curso.delete()
     return redirect('/')
+
+
+def editarCurso(request, codigo):
+    curso = Curso.objects.get(codigo=codigo)
+    return render(request, 'editarCurso.html', {'curso': curso})
+
+
+def edicionCurso(request):
+    codigo = request.POST['txtCodigo']
+    nombre = request.POST['txtNombre']
+    creditos = request.POST['numCreditos']
+
+    curso = Curso.objects.get(codigo=codigo)
+    curso.nombre = nombre
+    curso.creditos = creditos
+    curso.save()
+    return redirect('/')
