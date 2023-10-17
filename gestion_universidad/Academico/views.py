11c8af3b67ba65066ee6ec3d90f15f7367b84f3e
JuanGomez-14 <jmgomezc14@gmail.com>
2023-10-17 14:06:58 -0500

    Feat: Se muestra el listado de cursos agregados en la pagina principal

--- gestion_universidad/Academico/views.py
+++ gestion_universidad/Academico/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Curso
 
 
 def home(request):
-    return render(request, 'gestionCursos.html')
+    allCursos = Curso.objects.all()
+    return render(request, 'gestionCursos.html', {"cursos": allCursos})
