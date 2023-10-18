0587ee0cf33c5c64740801373d815e924020bcee
JuanGomez-14 <jmgomezc14@gmail.com>
2023-10-17 21:28:02 -0500

    Se revierten los cambios del commit anterior

--- gestion_universidad/gestion_universidad/settings.py
+++ gestion_universidad/gestion_universidad/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-xzzz84xl!=#ynp%)ti79^m#8er(@v0gc$jir8g+dov+@3$$k^&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = []
 
 
 # Application definition
